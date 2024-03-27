0c149681dd1b657c06ab9889fcc79f8f0dcba0f4
Lucas Dias <86629761+LucasDias09@users.noreply.github.com>
2024-03-27 15:41:00 +0000

    Create config.js
    create api config

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+// Unsplash API
+let count = 5;
+const apiKey = "2azDMgCkOupQClotZw26x9bp7l1UPd66FR3GD6ML8BQ";
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+ }
