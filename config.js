a000c9c388f03ac865ba952eacc098c9b65fd85c
Lucas Dias <86629761+LucasDias09@users.noreply.github.com>
2024-03-27 15:54:35 +0000

    Update config.js
    reitrar as declarações

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
 // Unsplash API
-let count = 5;
-const apiKey = "2azDMgCkOupQClotZw26x9bp7l1UPd66FR3GD6ML8BQ";
-let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+count = 5,
+apiKey = "2azDMgCkOupQClotZw26x9bp7l1UPd66FR3GD6ML8BQ",
+apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
  }
